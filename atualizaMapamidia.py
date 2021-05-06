4d4b98c0e67328fa3be66a964545df857ba16195
zorrex82 <edinorscjr@gmail.com>
2021-05-06 10:39:43 -0400

    Correção do comando curl

--- atualizaMapamidia.py
+++ atualizaMapamidia.py
@@ -2,7 +2,6 @@ import os
 
 url = 'https://api-ad-auth.irede.net/Usuario/GetAdUsers'
 
-arquivo = os.system("curl -o 'arquivo.json' https://api-ad-auth.irede.net/Usuario/GetAdUsers -H 'Accept: "
-                    "application/json'")
+arquivo = os.system("curl -o 'arquivo.json' https://api-ad-auth.irede.net/Usuario/GetAdUsers")
 
 #curl -o "arquivo-teste.json" https://api-ad-auth.irede.net/Usuario/GetAdUsers?carregarGerente=true -H "Accept: application/json"
