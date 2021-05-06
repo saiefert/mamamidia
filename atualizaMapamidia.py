b7a3b72ad88b102a3217124c00b871de04b14954
zorrex82 <edinorscjr@gmail.com>
2021-05-06 10:28:20 -0400

    alteração da porta padrão e configuração do curl para atualização do arquivo json

--- atualizaMapamidia.py
+++ atualizaMapamidia.py
@@ -2,6 +2,7 @@ import os
 
 url = 'https://api-ad-auth.irede.net/Usuario/GetAdUsers'
 
-arquivo = os.system("curl -o 'arquivo.json' {} -H 'Accept: application/json'".format(url))
+arquivo = os.system("curl -o 'arquivo.json' https://api-ad-auth.irede.net/Usuario/GetAdUsers -H 'Accept: "
+                    "application/json'")
 
 #curl -o "arquivo-teste.json" https://api-ad-auth.irede.net/Usuario/GetAdUsers?carregarGerente=true -H "Accept: application/json"
